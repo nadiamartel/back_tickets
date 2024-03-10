fbc8f8d6c744f9f35305970d8070dd86640d905b
nadiamartel <nadiagmartel@gmail.com>
2024-03-09 21:21:54 -0300

    db models

---
+++ src/models/User.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("user", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    client: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    picture: {
+      type: DataTypes.STRING(1000),
+      allowNull: true,
+    },
+
+  },
+  //para poder bannear a los users
+    {
+      paranoid: true
+    }
+  );
+};
