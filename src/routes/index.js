5f1e4bd32c9dcc33a999ebc821284781af1891c2
nadiamartel <nadiagmartel@gmail.com>
2024-03-11 19:42:41 -0300

    map router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 
-const userRouter = require()
+const userRouter = require("./user");
+const eventRouter = require("./event")
+
+router.use("/user", userRouter)
+router.use("/event", eventRouter)
+
+module.exports = router;
