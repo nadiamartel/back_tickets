9a005502462074c08b83233085df4b1b694c4f93
nadiamartel <nadiagmartel@gmail.com>
2024-03-12 20:48:14 -0300

    models

---
+++ src/models/Event.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) =>{
+    sequelize.define([
+        'require',
+        'dependency'
+    ], function(require, factory) {
+        'use strict';
+        
+    });
+}
+
+//pedir a Fer los campos
